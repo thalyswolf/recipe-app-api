4f6e1fab7ccef28de2e5eb9fa893863ad2fdac88
Thalys A Wolf <thalyswolf01@gmail.com>
2024-06-09 10:03:06 -0300

    lin

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -6,6 +6,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
@@ -19,7 +20,7 @@ class CommandTests(SimpleTestCase):
     def test_wait_fo_db_delay(self, patched_sleep, patched_check):
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
